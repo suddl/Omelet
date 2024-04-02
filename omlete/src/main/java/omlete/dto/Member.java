e2b404f168dccee6a12d1b3fc4742917846035e9
itwill <itwill@DESKTOP-VEPN1JC>
2024-04-02 19:27:44 +0900

    2024.04.02 지원_member_dto

---
+++ omlete/src/main/java/omlete/dto/Member.java
@@ -0,0 +1,24 @@
+package omlete.dto;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private int memberNo;
+	private String memberId;
+	private String memberPasswd;
+	private String memberName;
+	private String memberPhone;
+	private String memberBirth;
+	private String memberEmail;
+	private int memberLevel;
+	private int memberBadge;
+	private int memberStatus;
+	private int memberPoint;
+	private String memberFavorite1;
+	private String memberFavorite2;
+	private String memberFavorite3;
+	private int memberReport;
+	private int memberNickname;
+}
