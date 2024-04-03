f4fe23bc8f9ce9c852eeb9ca9f40e4f240e38114
itwill <itwill@DESKTOP-OP3B1FN>
2024-04-03 19:29:51 +0900

    dto 2개 추가 -김민준

--- omlete/src/main/java/omlete/dto/Notice.java
+++ omlete/src/main/java/omlete/dto/Notice.java
@@ -2,6 +2,18 @@ package omlete.dto;
 
 import lombok.Data;
 
+/*
+create table notice(
+notice_no NUMBER PRIMARY KEY,
+notice_title NUMBER,
+notice_content VARCHAR2(3000),
+notice_count NUMBER,
+notice_date DATE,
+notice_status NUMBER,
+notice_term DATE,
+notice_image VARCHAR2(100));
+*/
+
 @Data
 public class Notice {
 	private int noticeNo;
--- omlete/src/main/java/omlete/dto/Wish.java
+++ omlete/src/main/java/omlete/dto/Wish.java
@@ -3,6 +3,13 @@ package omlete.dto;
 import lombok.Builder;
 import lombok.Data;
 
+/*
+create table wish(
+wish_num NUMBER PRIMARY KEY,
+wish_client_num NUMBER CONSTRAINT wish_client_num_FK REFERENCES member(member_no),
+wish_work_num NUMBER CONSTRAINT wish_work_num_FK REFERENCES contents(contents_no));
+*/
+
 @Data
 public class Wish {
 	private int wishNo; // 찜 번호
