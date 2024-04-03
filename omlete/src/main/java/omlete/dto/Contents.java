e52cece6e037f7b260dbaf20e742915a5e2b9e92
itwill <itwill@DESKTOP-DUO7HM8>
2024-04-03 18:13:50 +0900

    04/03 dto 변경, 정세희

--- omlete/src/main/java/omlete/dto/Contents.java
+++ omlete/src/main/java/omlete/dto/Contents.java
@@ -7,25 +7,24 @@ import lombok.Data;
 //=> 페이지 요청시 전달값을 저장할 목적으로 사용 - 전달값의 이름과 필드의 이름이 같도록 작성
 @Data
 public class Contents {
-	private int contentsNo;				//작품번호
+	private int contentsNo;					//작품번호
 	private String contentsType;			//작품 타입
 	private String contentsOname;			//원제목(영어)
 	private String contentsTname;			//작품 제목(한국어)
-	private String contentsTrailer;		//예고편/트레일러
+	private String contentsTrailer;			//예고편/트레일러
 	private String contentsPoster;			//포스터
 	private String contentsPhoto;			//스틸컷
-	private String contentsCast;			//출연진(배우)
 	private String contentsStaff;			//작가
 	private String contentsOverview;		//내용
 	private String contentsRating;			//연령 등급
 	private String contentsGenre;			//장르
-	private String contentsNetwork;		//방송사
-	private String contentsNetworkLogo;	//방송사 로고
+	private String contentsNetwork;			//방송사
+	private String contentsNetworkLogo;		//방송사 로고
 	private String contentsCountries;		//제작 국가
 	private String contentsDirector;		//감독
 	private String contentsStartdate;		//개봉일
 	private int contentsAvgstar;			//평균별점
-	private String contentsEnddate;		//종영일
+	private String contentsEnddate;			//종영일
 	private int contentsEpisodes;			//총 에필로그 수
 	private int contentsSeasons;			//총 시즌 수
 	private int contentsRuntime;			//상영시간
