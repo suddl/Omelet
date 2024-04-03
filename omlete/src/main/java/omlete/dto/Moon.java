299b6e57d5b4d99317459ed59a5ea1e104df929e
itwill <itwill@DESKTOP-VEPN1JC>
2024-04-03 19:28:50 +0900

    0403 지원 DTO

--- omlete/src/main/java/omlete/dto/Moon.java
+++ omlete/src/main/java/omlete/dto/Moon.java
@@ -2,6 +2,16 @@ package omlete.dto;
 
 import lombok.Data;
 
+/* create table moon(moon_no number primary key,
+moon_member number CONSTRAINT moon_member_fk REFERENCES member(member_no),
+moon_title varchar2(100),
+moon_content varchar2(300),
+moon_status number,
+moon_re varchar2(300),
+moon_date date);
+
+*/
+
 @Data
 public class Moon {
 	private int moonNo;
