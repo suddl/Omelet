d33b809e13c5a784b6d94a7933b2da95761c7712
itwill <itwill@DESKTOP-UQ8T0AL>
2024-04-04 21:03:11 +0900

    0404 - Mapper, DAO 수정 중 - 조현규

---
+++ omlete/src/main/java/omlete/dao/ReviewDAO.java
@@ -0,0 +1,10 @@
+package omlete.dao;
+
+import omlete.dto.Review;
+
+public interface ReviewDAO {
+	/*
+	int insertReview(Review review);
+	int delete
+	*/
+}
--- omlete/src/main/java/omlete/mapper/ReviewMapper.java
+++ omlete/src/main/java/omlete/mapper/ReviewMapper.java
@@ -1,5 +1,15 @@
 package omlete.mapper;
 
-public interface ReviewMapper {
+import java.util.List;
 
+import omlete.dto.Review;
+
+public interface ReviewMapper {
+	/*
+	int insertReview(Review review);
+	int deleteReview(int reviewNo);
+	int updateReview(Review review);
+	Review selectReview(int reviewNo);
+	List<Review> selectReviewList
+	*/
 }
