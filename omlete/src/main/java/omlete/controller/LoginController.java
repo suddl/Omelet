91005cf2a8a39d8380772b23a240d09df3fe7264
itwill <itwill@DESKTOP-VEPN1JC>
2024-04-08 20:44:32 +0900

    04.08 지원 로그인

--- omlete/src/main/java/omlete/controller/LoginController.java
+++ omlete/src/main/java/omlete/controller/LoginController.java
@@ -12,41 +12,26 @@ import omlete.dto.Member;
 import omlete.exception.LoginAuthFailException;
 import omlete.service.MemberService;
 
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
-	
+
 	private final MemberService memberService;
-	
-	/**
-	 * 로그인 페이지 이동 - 권한없음 , 예외없음
-	 * @return
-	 */
-	
-	@RequestMapping(value = "/member",method = RequestMethod.GET)
+
+
+	@RequestMapping(value = "/member", method = RequestMethod.GET)
 	public String memberLogin() {
 		return "login/login";
+		
 	}
 	
-	/**
-	 * 로그인 처리 - 권한없음, 전달받은 아이디와 비밀번호에 대한 인증이 실패되면 예외처리, 인증 성공하면 session에 저장
-	 * @param member
-	 * @param session
-	 * @return
-	 * @throws LoginAuthFailException
-	 */
-	
-	@RequestMapping(value = "/member",method = RequestMethod.POST)
+	@RequestMapping(value = "/member", method = RequestMethod.POST)
 	public String memberLogin(@ModelAttribute Member member, HttpSession session) throws LoginAuthFailException {
 		Member loginMember = memberService.loginAuth(member);
 		session.setAttribute("loginMember", loginMember);
-		return "main/main_body";
+		// 로그인이 성공하면 메인 화면으로 이동
+		return "/main/main_body";
 	}
+	
 }
-
-	
-	
-
-
