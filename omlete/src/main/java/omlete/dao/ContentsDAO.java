af45388fcad35fa164c38ce172c3b856a70a84c9
itwill <itwill@DESKTOP-R92F6U5>
2024-04-09 17:50:47 +0900

    240409 김기윤 contentsDAO,DAOImpl,Mapper 수정

--- omlete/src/main/java/omlete/dao/ContentsDAO.java
+++ omlete/src/main/java/omlete/dao/ContentsDAO.java
@@ -12,4 +12,19 @@ public interface ContentsDAO {
 	 Contents selectContents(int contentsNo);
 	 //TMDB에서 작품 정보를 불러오기
 	 List<Contents> getContentsInfoList(String type);
+	 
+	 //타입별 출력
+	 List<Contents> selectContentsListByType(String contentsType);
+	 
+	 //장르별 개수 출력
+	 int selectContentsCountByGenre(String contentsGenre);
+	 
+	 //타입별 개수 출력
+	 int selectContentsCountByType(String contentsType);
+	 
+	 //작품 수정
+	 int updateContents(Contents contents);
+	 
+	 //작품 삭제
+	 int deleteContents(int contentsNo);	 
 }
--- omlete/src/main/java/omlete/mapper/ContentsMapper.java
+++ omlete/src/main/java/omlete/mapper/ContentsMapper.java
@@ -12,4 +12,18 @@ public interface ContentsMapper {
 	 //작품id로 해당 작품정보 불러오기
 	 Contents selectContents(int contentsNo);
 	 
+	 //타입별 출력
+	 List<Contents> selectContentsListByType(String contentsType);
+	 
+	 //장르별 개수 출력
+	 int selectContentsCountByGenre(String contentsGenre);
+	 
+	 //타입별 개수 출력
+	 int selectContentsCountByType(String contentsType);
+	 
+	 //작품 수정
+	 int updateContents(Contents contents);
+	 
+	 //작품 삭제
+	 int deleteContents(int contentsNo);
 }
