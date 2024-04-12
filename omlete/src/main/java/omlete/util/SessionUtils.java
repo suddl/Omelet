af68b4608119a46504212da7debe1ee555ca17f6
TTimotti <ttimotti@kakao.com>
2024-04-12 22:57:47 +0900

    로그인 사용자 정보를 가져오는 유틸 추가

---
+++ omlete/src/main/java/omlete/util/SessionUtils.java
@@ -0,0 +1,20 @@
+package omlete.util;
+
+import javax.servlet.http.HttpSession;
+
+import omlete.dto.Member;
+import omlete.exception.BadRequestException;
+
+public class SessionUtils {
+	
+    public static Member getMemberFromSession(HttpSession session) {
+        Member loginMember = (Member) session.getAttribute("loginMember");
+
+        if (loginMember == null) {
+            
+        	throw new BadRequestException("로그인이 필요한 기능입니다.");
+        }
+
+        return loginMember;
+    }
+}
