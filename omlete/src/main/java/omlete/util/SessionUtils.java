7c68bce017c43d762b021fa906b71e00863f6f4a
TTimotti <ttimotti@kakao.com>
2024-04-12 23:52:31 +0900

    Update SessionUtils.java
    - 포매팅 정렬

--- omlete/src/main/java/omlete/util/SessionUtils.java
+++ omlete/src/main/java/omlete/util/SessionUtils.java
@@ -6,15 +6,15 @@ import omlete.dto.Member;
 import omlete.exception.BadRequestException;
 
 public class SessionUtils {
-	
+
 	public static Member getMemberFromSession(HttpSession session) {
-        Member loginMember = (Member) session.getAttribute("loginMember");
+		Member loginMember = (Member) session.getAttribute("loginMember");
 
-        if (loginMember == null) {
-            
-        	throw new BadRequestException("로그인이 필요한 기능입니다.");
-        }
+		if (loginMember == null) {
 
-        return loginMember;
-    }
+			throw new BadRequestException("로그인이 필요한 기능입니다.");
+		}
+
+		return loginMember;
+	}
 }
